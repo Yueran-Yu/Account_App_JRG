d45711d675d3708cd43f3d848600f1f7b66f29be
GraceYu <librayuyueran@gmail.com>
2021-07-06 21:51:19 -0500

    add comment of using svg symbols

--- src/App.tsx
+++ src/App.tsx
@@ -30,3 +30,14 @@ function App() {
 }
 
 export default App;
+
+
+/*
+1 确定页面 url
+2 添加代码
+3 Route 有两种模式： HISTORY 和 Hash， 没有后台服务器就用Hash
+4 使用SVG Symbols
+    自定义 webpack config
+    1）yarn eject 拿到 webpack 配置 （注意git commit 一下）
+    2）然后根据 svg-sprite-loader的文档进行CRM （也要git commit）
+ */
