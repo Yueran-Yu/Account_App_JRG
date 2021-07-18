eba4cd7b9802d1ebb881d693de7b506a3998e019
GraceYu <librayuyueran@gmail.com>
2021-07-18 03:01:41 -0500

    complate the add and subtract, and delete function

--- src/components/AddMoney/NumberPadSection.tsx
+++ src/components/AddMoney/NumberPadSection.tsx
@@ -3,14 +3,27 @@ import React, {useState} from 'react';
 import {Icon} from "../Icon";
 
 const Wrapper = styled.section`
-  .output {
+  cursor: pointer;
+
+  .outputWrapper {
+    display: flex;
+    flex-direction: column;
     background: white;
-    font-size: 38px;
     color: #8d8d8d;
-    line-height: 56px;
     text-align: right;
     padding: 0 16px;
     box-shadow: inset 0 -3px 3px -3px rgba(0, 0, 0, 0.25);
+
+    .output {
+      font-size: 38px;
+      height: 35px;
+      padding-bottom: 2px;
+    }
+
+    .total {
+      line-height: 15px;
+      padding-bottom: 6px;
+    }
   }
 
   .pad {
@@ -43,15 +56,14 @@ const Wrapper = styled.section`
 `
 export const NumberPadSection = () => {
     const [output, _setOutput] = useState('0')
+    const [total, setTotal] = useState('')
     const [btn, setBtn] = useState('OK')
-    let temp = ''
-    let totaltemp = ''
 
     const setOutput = (output: string) => {
         if (output.length > 16) {
             output = output.slice(0, 16)
         } else if (output.length === 0) {
-            output = '0'
+            output = ''
         }
         _setOutput(output)
     }
@@ -59,72 +71,91 @@ export const NumberPadSection = () => {
     const onClickButtonWrapper = (e: React.MouseEvent) => {
         const text = (e.target as HTMLButtonElement).textContent
         if (text === null) {return}
-        switch (text) {
-            case '0':
-            case '1':
-            case '2':
-            case '3':
-            case '4':
-            case '5':
-            case '6':
-            case '7':
-            case '8':
-            case '9':
-                if (output === '0') {
-                    setOutput(text)
-                } else {
-                    setOutput(output + text)
-                }
-                break;
-            case '':
-                if (output.length === 1) {
+
+        try {
+            switch (text) {
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+
+                    if (output === '0') {
+                        setOutput(text)
+                        setTotal(text)
+                    } else {
+                        if (total === '') {
+                            setOutput(text)
+                            setTotal(text)
+                        } else {
+                            setOutput(output + text)
+                            setTotal(total + text)
+                        }
+                    }
+                    break;
+                case '':
+                    if (total.length > 1) {
+                        if (output === '') {
+                            setTotal(total.slice(0, -1))
+                        } else {
+                            setOutput(output.slice(0, -1))
+                            setTotal(total.slice(0, -1))
+                        }
+                    } else {
+                        setOutput('0')
+                        setTotal('')
+                    }
+                    break;
+                case '+':
+                    setBtn('=')
+                    setTotal(total + text)
                     setOutput('')
-                } else {
-                    setOutput(output.slice(0, -1))
-                }
-
-                break;
-            case '+':
-                setBtn('=')
-                // const lastLetter = (output.length - 1)
-                // const secLastLetter = (output.length - 2)
-                // console.log("last " + output.charAt(lastLetter))
-                // console.log("sec " + output.charAt(secLastLetter))
-
-                if (output === '0') {
-                    temp = output
-                    console.log(temp)
-                    totaltemp = temp + text
-                    console.log(totaltemp)
+                    break;
+                case '-':
+                    setBtn('=')
+                    setTotal(total + text)
+                    setOutput('')
+                    break;
+                case 'C':
                     setOutput('0')
-                } else {
-                    setOutput(output + text)
-                }
-                break;
-            case '-':
-                console.log(text)
-                break;
-            case 'C':
-                setOutput('')
-                break;
-            case '.':
-                if (output.indexOf('.') >= 0) {
-                    return;
-                } else {
-                    setOutput(output + '.')
-                }
-                break;
-            case 'OK':
-                console.log(text)
-                break;
-            default:
-                break;
+                    setTotal('')
+                    break;
+                case '.':
+                    if (output.indexOf('.') >= 0) {
+                        return;
+                    } else {
+                        setTotal(total + text)
+                        setOutput(output + '.')
+                    }
+                    break;
+                case 'OK':
+                    console.log(output)
+                    break;
+                case '=':
+                    setBtn('OK')
+                    setOutput(eval(total).toFixed(9).toString().replace(/(\.0*|(?<=(\..*))0*)$/, ''))
+                    setTotal('')
+                    break;
+                default:
+                    break;
+            }
+        } catch (e) {
+            console.log(e.message())
         }
+
     }
     return (
         <Wrapper>
-            <div className='output'>
-                {output}
+            <div className='outputWrapper'>
+                <div className='output'>
+                    {output}
+                </div>
+                <span className='total'>{total}</span>
             </div>
             <div className='pad clearfix' onClick={onClickButtonWrapper}>
                 {/* button{$}*16 */}
@@ -145,7 +176,6 @@ export const NumberPadSection = () => {
                 <button>C</button>
                 <button>0</button>
                 <button>.</button>
-
                 <button className='ok'>{btn}</button>
             </div>
         </Wrapper>
