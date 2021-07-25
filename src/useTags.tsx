e2ce283559a13f77bc5a5c260f36d9c0c9dd1e6c
GraceYu <librayuyueran@gmail.com>
2021-07-25 03:40:08 -0500

    encapsulate a self defined Hook

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -1,5 +1,6 @@
 import {useState} from 'react';
 
+//encapsulate a self defined Hook
 export const useTags = () => {
 	const [tags, setTags] = useState<string[]>(['Residence', 'Clothing', 'Food', 'Transportation'])
 	return {
